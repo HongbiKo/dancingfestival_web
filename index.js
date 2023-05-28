6c6bf0548b4ad038e0e247434776e20a4e49a1db
HongbiKo <rhghdql666@gmail.com>
2023-05-28 20:53:41 +0900

    Add list slide function

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 "use strict";
 
+// nav slidemenu funciton
 $(function () {
   let targetMainMenu = $(".mainmenu");
 
@@ -138,4 +139,61 @@ window.onload = function () {
     moveSection();
   });
   init();
+
+  // judge list slide
+  const slides = document.querySelector(".slider");
+  const slideImg = document.querySelectorAll(".slider li");
+  let currentIdx = 0;
+  const slideCount = slideImg.length;
+  const prev = document.querySelector(".prev");
+  const next = document.querySelector(".next");
+
+  const SLIDE_WIDTH = 200;
+  const SLIDE_MARGIN = 100;
+
+  makeClone();
+  initfunction();
+
+  function makeClone() {
+    let cloneSlide_first = slideImg[0].cloneNode(true);
+    let cloneSlide_last = slides.lastElementChild.cloneNode(true);
+    slides.appendChild(cloneSlide_first);
+    slides.insertBefore(cloneSlide_last, slides.firstElementChild);
+  }
+
+  function initfunction() {
+    slides.style.width = (SLIDE_WIDTH + SLIDE_MARGIN) * (slideCount + 2) + "px";
+    slides.style.left = -(SLIDE_WIDTH + SLIDE_MARGIN) + "px";
+  }
+
+  next.addEventListener("click", function () {
+    if (currentIdx <= slideCount - 1) {
+      slides.style.left =
+        -(currentIdx + 2) * (SLIDE_WIDTH + SLIDE_MARGIN) + "px";
+      slides.style.transition = `${0.5}s ease-out`;
+    }
+    if (currentIdx === slideCount - 1) {
+      setTimeout(function () {
+        slides.style.left = -(SLIDE_WIDTH + SLIDE_MARGIN) + "px";
+        slides.style.transition = `${0}s ease-out`;
+      }, 500);
+      currentIdx = -1;
+    }
+    currentIdx += 1;
+  });
+
+  prev.addEventListener("click", function () {
+    if (currentIdx >= 0) {
+      slides.style.left = -currentIdx * (SLIDE_WIDTH + SLIDE_MARGIN) + "px";
+      slides.style.transition = `${0.5}s ease-out`;
+    }
+    if (currentIdx === 0) {
+      setTimeout(function () {
+        slides.style.left = -slideCount * (SLIDE_WIDTH + SLIDE_MARGIN) + "px";
+        slides.style.transition = `${0}s ease-out`;
+      }, 500);
+      currentIdx = slideCount;
+    }
+    currentIdx -= 1;
+  });
 };
